f725759e284aea76ed54d8333caa1022ceab28c8
John Distasio <john@sect0r7.net>
2016-03-16 22:08:22 -0400

    Expand install script.

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# TODO: Install script.
+dotfiles=~/.dotfiles
+
+github="https://github.com"
+
+git clone --recursive ${github}/sorin-ionescu/prezto.git ~/.zprezto
+
+git clone ${github}/rbenv/rbenv.git ~/.rbenv \
+    && git clone ${github}/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+# TODO: Install dotfiles.
