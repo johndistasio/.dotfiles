89218e865a91be521d9d8abee4edf33d202a57ff
John Distasio <john@sect0r7.net>
2020-12-26 23:23:12 -0500

    Support arbitrary dotdirs

--- install.sh
+++ install.sh
@@ -2,15 +2,37 @@
 # @param1 path to directory
 function link() {
   local dir="${1}"
+  local dryrun="${2}"
 
   if [[ ! -d "${dir}" ]]; then
     echo "Warning: '${dir}' is not a directory; skipping..."
     return
   fi
 
+  while read -r d; do
+    d="${d/$dir/$HOME}"
+    d="${d/$HOME\//$HOME/.}"
+
+    if [[ "${dryrun}" != "" ]]; then
+      echo "mkdir -> ${d}"
+    else
+      mkdir -p "${d}"
+    fi
+
+  done < <(find "${dir}" -type d -mindepth 1)
+
   while read -r f; do
-    ln -v -s -f "${f}" "${HOME}/.$(basename ${f})"
-  done < <(find "${dir}" -type f)
+    l="${f/$dir/$HOME}"
+    l="${l/$HOME\//$HOME/.}"
+
+
+    if [[ "${dryrun}" != "" ]]; then
+      echo "'${l}' -> '${f}'"
+    else
+      ln -v -s -f "${f}" "${l}"
+    fi
+
+  done < <(find "${dir}" -type f -mindepth 1)
 }
 
 
@@ -20,4 +42,4 @@ if [[ -z "${DOTS}" ]]; then
   DOTS="${PWD}/home"
 fi
 
-(link "${DOTS}") | column -t
+(link "${DOTS}" "${DRYRUN}") | column -t
